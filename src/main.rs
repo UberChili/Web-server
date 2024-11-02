fd28e4cc3fb4e04f2ac70948e4197830cb98142b
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2024-11-02 16:19:31 -0600

    Parsing request data into a struct and implemented Display for Request

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,86 @@
 use std::{
-    io::{self, Read},
+    fmt::Display,
+    io::{self, BufRead, BufReader},
     net::{self, TcpStream},
 };
 
-fn handle_client(mut stream: TcpStream) -> io::Result<()> {
-    println!("Received connection: {:?}", stream);
+struct Request {
+    method: String,
+    path: String,
+    version: String,
+    host: String,
+    connection: String,
+}
 
-    let mut request: Vec<u8> = Vec::new();
-    let mut buffer = [0; 1024];
+impl Display for Request {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Request {{",)?;
+        writeln!(f, " Method: {}", self.method)?;
+        writeln!(f, " Path: {}", self.path)?;
+        writeln!(f, " Version: {}", self.version)?;
+        writeln!(f, " Host: {}", self.host)?;
+        writeln!(f, " Connection: {}", self.connection)?;
+        write!(f, "}}")
+    }
+}
 
-    loop {
-        match stream.read(&mut buffer) {
-            Ok(0) => break,
-            Ok(n) => {
-                request.extend_from_slice(&buffer[..n]);
-            }
-            Err(e) => return Err(e),
+fn handle_client(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    //println!("Request: {:#?}", http_request);
+
+    let request = parse_request(http_request).unwrap();
+    println!("{}", request)
+}
+
+fn parse_request(req: Vec<String>) -> Result<Request, io::Error> {
+    let mut method: String = String::new();
+    let mut path: String = String::new();
+    let mut version: String = String::new();
+    let mut host: String = String::new();
+    let mut connection: String = String::new();
+
+    for line in req {
+        if line.contains("HTTP") {
+            let method_line: Vec<_> = line
+                .split_whitespace()
+                .map(|word| word.to_string())
+                .collect();
+            method = method_line[0].clone();
+            path = method_line[1].clone();
+            version = method_line[method_line.len() - 1].clone();
+        } else if line.contains("Host:") {
+            let host_line: Vec<_> = line
+                .split_whitespace()
+                .map(|word| word.to_string())
+                .collect();
+            host = host_line[host_line.len() - 1].clone();
+        } else if line.contains("Connection:") {
+            let connection_line: Vec<_> = line
+                .split_whitespace()
+                .map(|word| word.to_string())
+                .collect();
+            connection = connection_line[connection_line.len() - 1].clone();
         }
     }
-
-    match String::from_utf8(request) {
-        Ok(request_str) => println!("Raw request:\n{}", request_str),
-        Err(e) => println!("Invalid UTF-8 sequence: {}", e),
-    }
-
-    Ok(())
+    Ok(Request {
+        method,
+        path,
+        version,
+        host,
+        connection,
+    })
 }
 
 fn main() -> io::Result<()> {
-    let my_listener = net::TcpListener::bind("127.0.0.1:8080")?;
+    let my_listener = net::TcpListener::bind("127.0.0.1:7878")?;
 
     for stream in my_listener.incoming() {
-        handle_client(stream?)?;
+        handle_client(stream.unwrap());
     }
 
     Ok(())
